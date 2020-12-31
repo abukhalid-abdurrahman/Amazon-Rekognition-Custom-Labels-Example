017d1ef7998e2e26d92df3a6a22277d9c5ed83a7
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2020-12-31 23:03:57 +0500

    Add project files.

---
+++ Amazon Rekognition Custom Labels Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Amazon_Rekognition_Custom_Labels_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
